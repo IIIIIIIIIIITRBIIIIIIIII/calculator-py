d373def7c9805a14075bde3f5d4eb446d0b50a9b
IIIIIIIIIIITRBIIIIIIIII <ouederniahmed844@gmail.com>
2025-07-08 07:29:49 -0700

    first commit

---
+++ calcul.py
@@ -0,0 +1,45 @@
+a = float(input("enter a number a"))
+b = float(input("enter a number b"))
+minus =input("minus(-) Y/N")
+plus =input("plus(+) Y/N")
+multiple =input("multiple(*) Y/N")
+devide =input("devide(/) Y/N")
+power =input("minus(**) Y/N")
+
+if minus == "Y" or minus == "y":
+    print( a - b)
+elif minus == "N" or minus == "n":
+    print("NULL")
+else:
+    print("enter a nuber")
+
+if plus == "Y" or plus == "y":
+    print( a + b)
+elif plus == "N" or plus == "n":
+    print("NULL")
+else:
+    print("enter a nuber")
+
+if multiple == "Y" or multiple == "y":
+    print( a * b)
+elif multiple == "N" or multiple == "n":
+    print("NULL")
+else:
+    print("enter a nuber")
+
+if devide == "Y" or devide == "y":
+    if b != 0:
+        print("Division result:", a / b)
+    else:
+        print("Error: Cannot divide by zero")
+elif devide == "N" or devide == "n":
+    print("NULL")
+else:
+    print("enter a nuber")
+
+if power == "Y" or power == "y":
+    print( a ** b)
+elif power == "N" or power == "n":
+    print("NULL")
+else:
+    print("enter a nuber")
